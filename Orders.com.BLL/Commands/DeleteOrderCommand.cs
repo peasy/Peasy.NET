218f4e966eaf258be4e158e379a079afa3222d18
Aaron Hanusa <aaron.hanusa@gmail.com>
2015-07-14 21:08:02 -0500

    Remove redundant transaction support interface

---
+++ Orders.com.BLL/Commands/DeleteOrderCommand.cs
@@ -0,0 +1,13 @@
+﻿using Facile.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orders.com.BLL.Commands
+{
+    public class DeleteOrderCommand : CommandBase
+    {
+    }
+}
