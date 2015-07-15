55121df12b886f37f25397ac232f8707379af986
Aaron Hanusa <aaron.hanusa@gmail.com>
2015-07-14 21:01:38 -0500

    Add transaction support

---
+++ Facile/Transaction/DTCTransactionContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace Phoebe.Core.Transaction
+{
+    public class DTCTransactionContext : ITransactionContext
+    {
+        public void Execute(Action codeToRunWithinTransaction)
+        {
+            using (var transactionScope = new TransactionScope(TransactionScopeOption.Required, new System.TimeSpan(0, 5, 0)))
+            {
+                codeToRunWithinTransaction();
+                transactionScope.Complete();
+            }
+        }
+
+        public T Execute<T>(Func<T> codeToRunWithinTransaction)
+        {
+            using (var transactionScope = new TransactionScope())
+            {
+                var result = codeToRunWithinTransaction();
+                transactionScope.Complete();
+                return result;
+            }
+        }
+
+        public async Task<T> ExecuteAsync<T>(Func<Task<T>> codeToRunWithinTransaction)
+        {
+            using (var transactionScope = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled))
+            {
+                var result = await codeToRunWithinTransaction();
+                transactionScope.Complete();
+                return result;
+            }
+        }
+
+        public async Task ExecuteAsync(Func<Task> codeToRunWithinTransaction)
+        {
+            using (var transactionScope = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled))
+            {
+                await codeToRunWithinTransaction();
+                transactionScope.Complete();
+            }
+        }
+    }
+}
---
+++ Facile/Transaction/ITransactionSupportStatusContainer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phoebe.Core.Transaction
+{
+    public interface ITransactionSupportStatusContainer
+    {
+        bool SupportsTransactions { get; }
+    }
+}
