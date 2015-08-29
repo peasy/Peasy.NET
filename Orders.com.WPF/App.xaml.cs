8dc0a83caf0d345caec4240f49f11c45479ce32c
Aaron Hanusa <aaron.hanusa@gmail.com>
2015-08-29 11:23:48 -0500

    Add global exception handling

--- Orders.com.WPF/App.xaml.cs
+++ Orders.com.WPF/App.xaml.cs
@@ -13,5 +13,16 @@ namespace Orders.com.WPF
     /// </summary>
     public partial class App : Application
     {
+        public App() : base()
+        {
+            this.Dispatcher.UnhandledException += Dispatcher_UnhandledException;
+        }
+
+        void Dispatcher_UnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            string errorMessage = string.Format("An unhandled exception occurred: {0}", e.Exception.Message);
+            MessageBox.Show(errorMessage, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            e.Handled = true;
+        }
     }
 }
