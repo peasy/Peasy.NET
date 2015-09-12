731a2b9e5a6772a3bf19a4752f901ad64be9f1db
Aaron Hanusa <aaron.hanusa@gmail.com>
2015-09-11 22:32:00 -0500

    Add code coverage for CommandBase

---
+++ Facile.Tests/CommandBaseTests.cs
@@ -0,0 +1,43 @@
+﻿using Facile.Core;
+using Shouldly;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using Xunit;
+
+namespace Facile.Tests.Rules
+{
+    [Trait("CommandBase", "CommandBase")]
+    public class CommandBaseTests
+    {
+        [Fact]
+        public void CanExecuteReturnsTrueWhenNoErrors()
+        {
+            var mock = new MockCommandBase();
+            mock.CanExecute.ShouldBe(true);
+        }
+
+        [Fact]
+        public void CanExecuteReturnsFalseWhenErrors()
+        {
+            var mock = new MockCommandBase();
+            mock.Errors = new[] { new ValidationResult("Object doesn't exist") };
+            mock.CanExecute.ShouldBe(false);
+        }
+    }
+
+    public class MockCommandBase : CommandBase
+    {
+        public MockCommandBase()
+        {
+            Errors = Enumerable.Empty<ValidationResult>();
+        }
+
+        public IEnumerable<ValidationResult> Errors { get; set; }
+
+        public override IEnumerable<ValidationResult> GetErrors()
+        {
+            return Errors;
+        }
+    }
+}
