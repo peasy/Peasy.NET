ab3253a54e41e11ae0ff32f05a70ce3e182237aa
Aaron Hanusa <aaron.hanusa@gmail.com>
2015-09-11 22:18:50 -0500

    Add code coverage for Command

---
+++ Facile.Tests/Rules/CommandTests.cs
@@ -0,0 +1,105 @@
+﻿using Facile.Core;
+using Shouldly;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Facile.Tests.Rules
+{
+    [Trait("Command", "Command")]
+    public class CommandTests
+    {
+        [Fact]
+        public void OnInitializationIsInvoked()
+        {
+            var mock = new MockCommand();
+            mock.Execute();
+            mock.OnInitializationInvoked.ShouldBe(true);
+        }
+
+        [Fact]
+        public void OnExecuteIsInvokedWhenNoErrorsExist()
+        {
+            var mock = new MockCommand();
+            mock.Execute();
+            mock.OnExecuteInvoked.ShouldBe(true);
+        }
+
+        [Fact]
+        public void OnExecuteIsNotInvokedWhenErrorsExist()
+        {
+            var mock = new MockCommand();
+            mock.Errors = new[] { new ValidationResult("Object doesn't exist") };
+            mock.Execute();
+            mock.OnExecuteInvoked.ShouldBe(false);
+        }
+
+        [Fact]
+        public void OnInitializationAsyncIsInvoked()
+        {
+            var mock = new MockCommand();
+            mock.ExecuteAsync().Wait();
+            mock.OnInitializationAsyncInvoked.ShouldBe(true);
+        }
+
+        [Fact]
+        public void OnExecuteAsyncIsInvokedWhenNoErrorsExist()
+        {
+            var mock = new MockCommand();
+            mock.ExecuteAsync().Wait();
+            mock.OnExecuteAsyncInvoked.ShouldBe(true);
+        }
+
+        [Fact]
+        public void OnExecuteAsyncIsNotInvokedWhenErrorsExist()
+        {
+            var mock = new MockCommand();
+            mock.Errors = new[] { new ValidationResult("Object doesn't exist") };
+            mock.ExecuteAsync().Wait();
+            mock.OnExecuteAsyncInvoked.ShouldBe(false);
+        }
+    }
+
+    public class MockCommand : Command
+    {
+        public MockCommand()
+        {
+            Errors = Enumerable.Empty<ValidationResult>();
+        }
+
+        public IEnumerable<ValidationResult> Errors { get; set; }
+        public bool OnInitializationInvoked { get; set; }
+        public bool OnExecuteInvoked { get; set; }
+        public bool OnInitializationAsyncInvoked { get; set; }
+        public bool OnExecuteAsyncInvoked { get; set; }
+
+        protected override void OnInitialization()
+        {
+            OnInitializationInvoked = true;
+        }
+
+        protected override Task OnInitializationAsync()
+        {
+            OnInitializationAsyncInvoked = true;
+            return Task.Delay(0);
+        }
+
+        public override IEnumerable<ValidationResult> GetErrors()
+        {
+            return Errors;
+        }
+
+        protected override void OnExecute()
+        {
+            OnExecuteInvoked = true;
+        }
+
+        protected override Task OnExecuteAsync()
+        {
+            OnExecuteAsyncInvoked = true;
+            return Task.Delay(0);
+        }
+    }
+}
