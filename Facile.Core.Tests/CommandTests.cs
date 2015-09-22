81435155b40ad66f9853d52d7015ca35afeace62
Aaron Hanusa <aaron.hanusa@gmail.com>
2015-09-21 20:43:14 -0500

    Add new Facile.Core.Tests using MS Tests

---
+++ Facile.Core.Tests/CommandTests.cs
@@ -0,0 +1,144 @@
+﻿using Facile.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Facile.Core.Tests
+{
+    [TestClass]
+    public class CommandTests
+    {
+        [TestMethod]
+        public void OnInitializationIsInvoked()
+        {
+            var mock = new MockCommand();
+            var result = mock.Execute();
+            mock.OnInitializationWasInvoked.ShouldBe(true);
+        }
+
+        [TestMethod]
+        public void OnExecuteIsInvokedWhenNoErrorsExist()
+        {
+            var mock = new MockCommand();
+            var result = mock.Execute();
+            mock.OnExecuteWasInvoked.ShouldBe(true);
+        }
+
+        [TestMethod]
+        public void ExecutionResultIsSuccessfulWhenValidationIsSuccessful()
+        {
+            var mock = new MockCommand();
+            var result = mock.Execute();
+            result.Success.ShouldBe(true); 
+        }
+
+        [TestMethod]
+        public void ExecutionResultShouldContainNoErrorsWhenValidationIsSuccessful()
+        {
+            var mock = new MockCommand();
+            var result = mock.Execute();
+            result.Errors.ShouldBe(null);
+        }
+
+        [TestMethod]
+        public void OnExecuteIsNotInvokedWhenErrorsExist()
+        {
+            var mock = new MockCommand();
+            mock.Errors = new[] { new ValidationResult("Object doesn't exist") };
+            mock.Execute();
+            mock.OnExecuteWasInvoked.ShouldBe(false);
+        }
+
+        [TestMethod]
+        public void ExecutionResultIsNotSuccessfulWhenValidationIsNotSuccessful()
+        {
+            var mock = new MockCommand();
+            mock.Errors = new[] { new ValidationResult("Object doesn't exist") };
+            var result = mock.Execute();
+            result.Success.ShouldBe(false); 
+        }
+
+        [TestMethod]
+        public void ExecutionResultShouldContainErrorsWhenValidationIsNotSuccessful()
+        {
+            var mock = new MockCommand();
+            mock.Errors = new[] { new ValidationResult("Object doesn't exist") };
+            var result = mock.Execute();
+            result.Errors.Count().ShouldBe(1);
+        }
+
+        [TestMethod]
+        public async Task OnInitializationAsyncIsInvoked()
+        {
+            var mock = new MockCommand();
+            await mock.ExecuteAsync();
+            mock.OnInitializationAsyncWasInvoked.ShouldBe(true);
+        }
+
+        [TestMethod]
+        public async Task OnExecuteAsyncIsInvokedWhenNoErrorsExist()
+        {
+            var mock = new MockCommand();
+            await mock.ExecuteAsync();
+            mock.OnExecuteAsyncWasInvoked.ShouldBe(true);
+       } 
+
+        [TestMethod]
+        public async Task OnExecuteAsyncIsNotInvokedWhenErrorsExist()
+        {
+            var mock = new MockCommand();
+            mock.Errors = new[] { new ValidationResult("Object doesn't exist") };
+            await mock.ExecuteAsync();
+            mock.OnExecuteAsyncWasInvoked.ShouldBe(false);
+        }
+    }
+
+    public class MockCommand : Command
+    {
+        public MockCommand()
+        {
+            Errors = Enumerable.Empty<ValidationResult>();
+        }
+
+        public IEnumerable<ValidationResult> Errors { get; set; }
+        public bool OnInitializationWasInvoked { get; set; }
+        public bool OnExecuteWasInvoked { get; set; }
+        public bool OnInitializationAsyncWasInvoked { get; set; }
+        public bool OnExecuteAsyncWasInvoked { get; set; }
+
+        protected override void OnInitialization()
+        {
+            OnInitializationWasInvoked = true;
+        }
+
+        protected override Task OnInitializationAsync()
+        {
+            OnInitializationAsyncWasInvoked = true;
+            return Task.FromResult(true);
+        }
+
+        public override IEnumerable<ValidationResult> GetErrors()
+        {
+            return Errors;
+        }
+
+        public override Task<IEnumerable<ValidationResult>> GetErrorsAsync()
+        {
+            return Task.Run(() => GetErrors());
+        }
+
+        protected override void OnExecute()
+        {
+            OnExecuteWasInvoked = true;
+        }
+
+        protected override Task OnExecuteAsync()
+        {
+            OnExecuteAsyncWasInvoked = true;
+            return Task.FromResult(true);
+        }
+    }
+}
---
+++ Facile.Core.Tests/RuleBaseTests.cs
@@ -0,0 +1,285 @@
+﻿using Facile.Core;
+using Facile.Core.Tests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+
+namespace Facile.Core.Tests
+{
+    [TestClass]
+    public class RuleBaseTests
+    {
+        [TestMethod]
+        public void ValidRuleIsValidAfterValidation()
+        {
+            var rule = new TrueRule().Validate();
+            rule.IsValid.ShouldBe(true);
+        }
+
+        [TestMethod]
+        public void ValidRuleDoesNotContainAnErrorMessageAfterValidation()
+        {
+            var rule = new TrueRule().Validate();
+            rule.ErrorMessage.ShouldBe(null);
+        }
+
+        [TestMethod]
+        public void InvalidRuleIsInvalidAfterValidation()
+        {
+            var rule = new FalseRule1().Validate();
+            rule.IsValid.ShouldBe(false);
+        }
+
+        [TestMethod]
+        public void InvalidRuleContainsAnErrorMessageAfterValidation()
+        {
+            var rule = new FalseRule1().Validate();
+            rule.ErrorMessage.ShouldBe("FalseRule1 failed validation");
+        }
+
+        [TestMethod]
+        public void ValidParentFailsWhenSuccessorFailsValidation()
+        {
+            var rule1 = new TrueRule().IfValidThenValidate(new FalseRule1()).Validate();
+            rule1.IsValid.ShouldBe(false);
+            rule1.ErrorMessage.ShouldBe("FalseRule1 failed validation");
+        }
+
+        [TestMethod]
+        public void SuccessorDoesNotExecuteWhenParentFails()
+        {
+            var rule1 = new FalseRule1().IfValidThenValidate(new FalseRule2()).Validate();
+            rule1.IsValid.ShouldBe(false);
+            rule1.ErrorMessage.ShouldBe("FalseRule1 failed validation");
+        }
+
+        [TestMethod]
+        public void AllRemainingSuccessorsSkipValidationWhenFirstSuccessorFails()
+        {
+            var rule1 = new TrueRule()
+                                .IfValidThenValidate(new FalseRule1(), new FalseRule2(), new FalseRule3())
+                                .Validate();
+            rule1.IsValid.ShouldBe(false);
+            rule1.ErrorMessage.ShouldBe("FalseRule1 failed validation");
+        }
+
+        [TestMethod]
+        public void LastSuccessorValidatesWhenFirstSuccessorsPass()
+        {
+            var rule1 = new TrueRule()
+                                .IfValidThenValidate(new TrueRule(), new TrueRule(), new FalseRule1())
+                                .Validate();
+            rule1.IsValid.ShouldBe(false);
+            rule1.ErrorMessage.ShouldBe("FalseRule1 failed validation");
+        }
+
+        [TestMethod]
+        public void LastSuccessorsInSuccessorChainAreSkippedWhenFirstSuccessorsFail()
+        {
+            var rule = new TrueRule()
+                              .IfValidThenValidate(new TrueRule(), new FalseRule1())
+                              .IfValidThenValidate(new FalseRule2(), new FalseRule3())
+                              .Validate();
+            rule.IsValid.ShouldBe(false);
+            rule.ErrorMessage.ShouldBe("FalseRule1 failed validation");
+        } 
+
+        [TestMethod]
+        public void LastSuccessorInSuccessorChainIsSkippedWhenFirstSuccessorsPass()
+        {
+            var rule = new TrueRule()
+                              .IfValidThenValidate(new TrueRule(), new TrueRule())
+                              .IfValidThenValidate(new FalseRule2(), new FalseRule3())
+                              .Validate();
+            rule.IsValid.ShouldBe(false);
+            rule.ErrorMessage.ShouldBe("FalseRule2 failed validation");
+        } 
+
+        [TestMethod]
+        public void ParentFailsWhenLastSuccessorInChainsFailsValidation()
+        {
+            var rule = new TrueRule()
+                              .IfValidThenValidate(new TrueRule(), new TrueRule())
+                              .IfValidThenValidate(new TrueRule(), new FalseRule3())
+                              .Validate();
+            rule.IsValid.ShouldBe(false);
+            rule.ErrorMessage.ShouldBe("FalseRule3 failed validation");
+        }
+
+        [TestMethod]
+        public void ThreeRuleChainExecutesSuccessfully()
+        {
+            var rule = new TrueRule()
+                            .IfValidThenValidate(new TrueRule()
+                                                      .IfValidThenValidate(new TrueRule())).Validate();
+
+            rule.IsValid.ShouldBe(true);
+        }
+
+        [TestMethod]
+        public void ThreeRuleChainFailSkipsThirdInChainWhenSecondFails()
+        {
+            var rule = new TrueRule()
+                            .IfValidThenValidate(new FalseRule1()
+                                                      .IfValidThenValidate(new FalseRule2())).Validate();
+            rule.IsValid.ShouldBe(false);
+            rule.ErrorMessage.ShouldBe("FalseRule1 failed validation");
+        }
+
+        [TestMethod]
+        public void ThreeRuleChainHitsThirdInChainAndFailsParent()
+        {
+            var rule = new TrueRule()
+                            .IfValidThenValidate(new TrueRule()
+                                                      .IfValidThenValidate(new FalseRule1())).Validate();
+            rule.IsValid.ShouldBe(false);
+            rule.ErrorMessage.ShouldBe("FalseRule1 failed validation");
+        }
+
+        [TestMethod]
+        public void InvokesIfValidThenExecute()
+        {
+            var output = string.Empty;
+            new TrueRule()
+                    .IfValidThenExecute(rule => output = "pass")
+                    .Validate();
+
+            output.ShouldBe("pass");
+        }
+
+        [TestMethod]
+        public void DoesNotInvokeIfValidThenExecute()
+        {
+            var output = string.Empty;
+            new FalseRule1()
+                    .IfValidThenExecute(rule => output = "pass")
+                    .Validate();
+
+            output.ShouldBe(string.Empty);
+        }
+
+        [TestMethod]
+        public void InvokesIfInvalidThenExecute()
+        {
+            var output = string.Empty;
+            new FalseRule1()
+                    .IfInvalidThenExecute(rule => output = "pass")
+                    .Validate();
+
+            output.ShouldBe("pass");
+        }
+
+        [TestMethod]
+        public void DoesNotInvokeIfInvalidThenExecute()
+        {
+            var output = string.Empty;
+            new TrueRule() 
+                   .IfInvalidThenExecute(rule => output = "pass")
+                   .Validate();
+
+            output.ShouldBe(string.Empty);
+        }
+
+        [TestMethod]
+        public void SuccessorInvokesExecuteIfValidThenExecute()
+        {
+            var output = string.Empty;
+            new TrueRule()
+                    .IfValidThenValidate(new TrueRule()
+                                                .IfValidThenExecute(r => output = "pass")).Validate();
+
+            output.ShouldBe("pass");
+        }
+
+        [TestMethod]
+        public void SuccessorDoesNotInvokeExecuteIfValidThenExecute()
+        {
+            var output = string.Empty;
+            new TrueRule()
+                .IfValidThenValidate(new FalseRule1()
+                                         .IfValidThenExecute(r => output = "pass")).Validate();
+
+            output.ShouldBe(string.Empty);
+        }
+
+        [TestMethod]
+        public void SuccessorInvokesExecuteIfInvalidThenExecute()
+        {
+            var output = string.Empty;
+            new TrueRule()
+                .IfValidThenValidate(new FalseRule1()
+                                         .IfInvalidThenExecute(r => output = "pass")).Validate();
+
+            output.ShouldBe("pass");
+        }
+
+        [TestMethod]
+        public void SuccessorDoesNotInvokeExecuteIfInvalidThenExecute()
+        {
+            var output = string.Empty;
+            new TrueRule()
+                .IfValidThenValidate(new TrueRule()
+                                         .IfInvalidThenExecute(r => output = "pass")).Validate();
+
+            output.ShouldBe(string.Empty);
+        }
+
+        [TestMethod]
+        public void FirstValidRuleInFirstSuccessorChainShouldExecute()
+        {
+            var output = string.Empty;
+            var rule = new TrueRule()
+                              .IfValidThenValidate(new TrueRule().IfValidThenExecute(r => output = "pass"), new TrueRule())
+                              .IfValidThenValidate(new TrueRule(), new FalseRule3())
+                              .Validate();
+            output.ShouldBe("pass");
+        }
+
+        [TestMethod]
+        public void SecondValidRuleInFirstSuccessorChainShouldExecute()
+        {
+            var output = string.Empty;
+            var rule = new TrueRule()
+                              .IfValidThenValidate(new TrueRule(), new TrueRule().IfValidThenExecute(r => output = "pass"))
+                              .IfValidThenValidate(new TrueRule(), new FalseRule3())
+                              .Validate();
+            output.ShouldBe("pass");
+        }
+
+        [TestMethod]
+        public void FirstValidRuleInSecondSuccessorChainShouldExecute()
+        {
+            var output = string.Empty;
+            var rule = new TrueRule()
+                              .IfValidThenValidate(new TrueRule(), new TrueRule())
+                              .IfValidThenValidate(new TrueRule().IfValidThenExecute(r => output = "pass"), new FalseRule3())
+                              .Validate();
+            output.ShouldBe("pass");
+        }
+
+        [TestMethod]
+        public void SecondInvalidRuleInSecondSuccessorChainShouldExecute()
+        {
+            var output = string.Empty;
+            var rule = new TrueRule()
+                              .IfValidThenValidate(new TrueRule(), new TrueRule())
+                              .IfValidThenValidate(new TrueRule(), new FalseRule3().IfInvalidThenExecute(r => output = "pass"))
+                              .Validate();
+            output.ShouldBe("pass");
+        }
+
+        [TestMethod]
+        public void FirstValidRuleInSecondSuccessorChainShouldExecuteButThirdFalseRuleShouldNot()
+        {
+            var output = string.Empty;
+            var output2 = string.Empty;
+            var rule = new TrueRule()
+                              .IfValidThenValidate(new TrueRule().IfValidThenExecute(r => output = "pass"), new TrueRule())
+                              .IfValidThenValidate(new FalseRule2().IfValidThenExecute(r => output2 = "pass"), new FalseRule3())
+                              .Validate();
+            output.ShouldBe("pass");
+            output2.ShouldBe(string.Empty);
+            rule.ErrorMessage.ShouldBe("FalseRule2 failed validation");
+        }
+    }
+
+}
