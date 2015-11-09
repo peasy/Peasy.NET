e505e32a0169ac87f1b7a4a5b3041ec6f79c361a
Aaron Hanusa <aaron.hanusa@gmail.com>
2015-11-09 13:45:49 -0600

    add state and behavior based tests

--- Orders.com.BLL.Tests/Commands/CreateProductCommandTests.cs
+++ Orders.com.BLL.Tests/Commands/CreateProductCommandTests.cs
@@ -19,6 +19,8 @@ namespace Orders.com.BLL.Tests.Commands
     [TestClass]
     public class CreateProductCommandTests
     {
+        #region State-Based Tests
+
         [TestMethod]
         public void Execution_should_fail_when_all_required_params_are_not_set()
         {
@@ -28,20 +30,6 @@ namespace Orders.com.BLL.Tests.Commands
             result.Success.ShouldBe(false);
             result.Errors.Count().ShouldBe(3);
         }
-
-        //[TestMethod]
-        //public void Execution_should_fail_when_all_required_params_are_not_set()
-        //{
-        //    var product = new Product();
-        //    var command = new CreateProductCommand(product,
-        //                                           Mock.Of<IProductDataProxy>(),
-        //                                           Mock.Of<IInventoryItemService>(),
-        //                                           new MockTransactionContext());
-
-        //    var result = command.Execute();
-        //    result.Success.ShouldBe(false);
-        //    result.Errors.Count().ShouldBe(3);
-        //}
 
         [TestMethod]
         public void Execution_should_succeed_when_all_required_params_are_set()
@@ -53,45 +41,62 @@ namespace Orders.com.BLL.Tests.Commands
             result.Errors.ShouldBeNull();
         }
 
-        //[TestMethod]
-        //public void Execution_should_succeed_when_all_required_params_are_set()
-        //{
-        //    var product = CreateValidProduct();
-
-        //    var productDataProxy = new Mock<IProductDataProxy>();
-        //    productDataProxy.Setup(p => p.Insert(product))
-        //             .Returns(product)
-        //             .Callback<Product>(p => product.ID = 10);
-
-        //    var inventoryDataProxy = new Mock<IInventoryItemDataProxy>();
-        //    inventoryDataProxy.Setup(dp => dp.Insert(It.IsAny<InventoryItem>()));
-
-        //    var command = new CreateProductCommand(product,
-        //                                           productDataProxy.Object,
-        //                                           new InventoryItemService(inventoryDataProxy.Object),
-        //                                           new MockTransactionContext());
-        //    var result = command.Execute();
-        //    result.Success.ShouldBe(true);
-        //}
-
-
-        //[TestMethod]
-        //public void Product_and_inventory_item_should_be_created()
-        //{
-        //    var product = CreateValidProduct();
-        //    var productDataProxy = new ProductRepository();
-        //    var inventoryDataProxy = new InventoryItemRepository();
-        //    var inventoryService = new InventoryItemService(inventoryDataProxy);
-        //    var command = new CreateProductCommand(product, productDataProxy, inventoryService, new MockTransactionContext());
-
-        //    var newProduct = command.Execute().Value;
-
-        //    productDataProxy.GetByID(newProduct.ID).ShouldNotBeNull();
-        //    inventoryDataProxy.GetByProduct(newProduct.ID).ShouldNotBeNull(); 
-        //}
-
         [TestMethod]
         public void Product_and_inventory_item_should_be_created()
+        {
+            var product = CreateValidProduct();
+            var productDataProxy = new ProductRepository();
+            var inventoryDataProxy = new InventoryItemRepository();
+            var inventoryService = new InventoryItemService(inventoryDataProxy);
+            var command = new CreateProductCommand(product, productDataProxy, inventoryService, new MockTransactionContext());
+
+            var newProduct = command.Execute().Value;
+
+            productDataProxy.GetByID(newProduct.ID).ShouldNotBeNull();
+            inventoryDataProxy.GetByProduct(newProduct.ID).ShouldNotBeNull();
+        }
+
+        #endregion
+
+        #region Behavior-Based Tests
+
+        [TestMethod]
+        public void Execution_should_fail_when_all_required_params_are_not_set_behavior_based()
+        {
+            var product = new Product();
+            var command = new CreateProductCommand(product,
+                                                   Mock.Of<IProductDataProxy>(),
+                                                   Mock.Of<IInventoryItemService>(),
+                                                   new MockTransactionContext());
+
+            var result = command.Execute();
+            result.Success.ShouldBe(false);
+            result.Errors.Count().ShouldBe(3);
+        }
+
+        [TestMethod]
+        public void Execution_should_succeed_when_all_required_params_are_set_behavior_based()
+        {
+            var product = CreateValidProduct();
+
+            var productDataProxy = new Mock<IProductDataProxy>();
+            productDataProxy.Setup(p => p.Insert(product))
+                     .Returns(product)
+                     .Callback<Product>(p => product.ID = 10);
+
+            var inventoryDataProxy = new Mock<IInventoryItemDataProxy>();
+            inventoryDataProxy.Setup(dp => dp.Insert(It.IsAny<InventoryItem>()));
+
+            var command = new CreateProductCommand(product,
+                                                   productDataProxy.Object,
+                                                   new InventoryItemService(inventoryDataProxy.Object),
+                                                   new MockTransactionContext());
+            var result = command.Execute();
+            result.Success.ShouldBe(true);
+        }
+
+        [TestMethod]
+        public void Product_and_inventory_item_should_be_created_behavior_based()
         {
             var product = CreateValidProduct();
 
@@ -112,6 +117,8 @@ namespace Orders.com.BLL.Tests.Commands
             inventoryDataProxy.Verify(p => p.Insert(It.Is<InventoryItem>(i => i.ProductID == 10)), Times.Once());
         }
 
+        #endregion
+
         private ICommand<Product> CreateCommand(Product product)
         {
             var productDataProxy = new ProductRepository();
@@ -128,20 +135,6 @@ namespace Orders.com.BLL.Tests.Commands
                 CategoryID = 1,
                 Price = 2000M
             };
-        }
-
-        private ICommand<Product> CreateCommandII(Product product, Mock<IProductDataProxy> productDataProxyMock, Mock<IInventoryItemDataProxy> inventoryDataProxyMock)
-        {
-            productDataProxyMock.Setup(p => p.Insert(product))
-                                .Callback<Product>(p => product.ID = 10)
-                                .Returns(product);
-
-            inventoryDataProxyMock.Setup(dp => dp.Insert(It.IsAny<InventoryItem>()));
-
-            return new CreateProductCommand(product,
-                                            productDataProxyMock.Object,
-                                            new InventoryItemService(inventoryDataProxyMock.Object),
-                                            new MockTransactionContext());
         }
     }
 }
