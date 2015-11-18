4078f935e8faf106fa8f4b6b4a7b2300b7757c98
Aaron Hanusa <aaron.hanusa@gmail.com>
2015-11-17 20:43:03 -0600

    Add tests for ServiceExceptionHandling and Command<T>

--- Peasy.Core.Tests/CommandTests.cs
+++ Peasy.Core.Tests/CommandTests.cs
@@ -5,14 +5,18 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Moq;
+using System;
 
 namespace Peasy.Core.Tests
 {
     [TestClass]
     public class CommandTests
     {
+        #region Command
+
         [TestMethod]
-        public void OnInitializationIsInvoked()
+        public void OnInitialization_Is_Invoked()
         {
             var mock = new MockCommand();
             var result = mock.Execute();
@@ -20,7 +24,7 @@ namespace Peasy.Core.Tests
         }
 
         [TestMethod]
-        public void OnExecuteIsInvokedWhenNoErrorsExist()
+        public void OnExecute_Is_Invoked_When_No_Errors_Exist()
         {
             var mock = new MockCommand();
             var result = mock.Execute();
@@ -28,7 +32,7 @@ namespace Peasy.Core.Tests
         }
 
         [TestMethod]
-        public void ExecutionResultIsSuccessfulWhenValidationIsSuccessful()
+        public void ExecutionResult_Is_Successful_When_Validation_Is_Successful()
         {
             var mock = new MockCommand();
             var result = mock.Execute();
@@ -36,7 +40,7 @@ namespace Peasy.Core.Tests
         }
 
         [TestMethod]
-        public void ExecutionResultShouldContainNoErrorsWhenValidationIsSuccessful()
+        public void ExecutionResult_Should_Contain_No_Errors_When_Validation_Is_Successful()
         {
             var mock = new MockCommand();
             var result = mock.Execute();
@@ -44,7 +48,7 @@ namespace Peasy.Core.Tests
         }
 
         [TestMethod]
-        public void OnExecuteIsNotInvokedWhenErrorsExist()
+        public void OnExecute_Is_Not_Invoked_When_Errors_Exist()
         {
             var mock = new MockCommand();
             mock.Errors = new[] { new ValidationResult("Object doesn't exist") };
@@ -53,7 +57,7 @@ namespace Peasy.Core.Tests
         }
 
         [TestMethod]
-        public void ExecutionResultIsNotSuccessfulWhenValidationIsNotSuccessful()
+        public void ExecutionResult_Is_Not_Successful_When_Validation_Is_Not_Successful()
         {
             var mock = new MockCommand();
             mock.Errors = new[] { new ValidationResult("Object doesn't exist") };
@@ -62,7 +66,7 @@ namespace Peasy.Core.Tests
         }
 
         [TestMethod]
-        public void ExecutionResultShouldContainErrorsWhenValidationIsNotSuccessful()
+        public void ExecutionResult_Should_Contain_Errors_When_Validation_Is_Not_Successful()
         {
             var mock = new MockCommand();
             mock.Errors = new[] { new ValidationResult("Object doesn't exist") };
@@ -71,7 +75,15 @@ namespace Peasy.Core.Tests
         }
 
         [TestMethod]
-        public async Task OnInitializationAsyncIsInvoked()
+        public void ExecutionResult_Should_Contain_Errors_When_ServiceException_Is_Caught()
+        {
+            var command = new MockCommandThrowsErrorsOnExecute();
+            var result = command.Execute();
+            result.Errors.Count().ShouldBe(1);
+        }
+
+        [TestMethod]
+        public async Task OnInitializationAsync_Is_Invoked()
         {
             var mock = new MockCommand();
             await mock.ExecuteAsync();
@@ -79,7 +91,7 @@ namespace Peasy.Core.Tests
         }
 
         [TestMethod]
-        public async Task OnExecuteAsyncIsInvokedWhenNoErrorsExist()
+        public async Task OnExecuteAsync_Is_Invoked_When_No_Errors_Exist()
         {
             var mock = new MockCommand();
             await mock.ExecuteAsync();
@@ -87,13 +99,143 @@ namespace Peasy.Core.Tests
        } 
 
         [TestMethod]
-        public async Task OnExecuteAsyncIsNotInvokedWhenErrorsExist()
+        public async Task OnExecuteAsync_Is_Not_Invoked_When_Errors_Exist()
         {
             var mock = new MockCommand();
             mock.Errors = new[] { new ValidationResult("Object doesn't exist") };
             await mock.ExecuteAsync();
             mock.OnExecuteAsyncWasInvoked.ShouldBe(false);
         }
+
+        [TestMethod]
+        public async Task ExecutionResult_Should_Contain_Errors_When_ServiceException_Is_Caught_Async()
+        {
+            var command = new MockCommandThrowsErrorsOnExecute();
+            var result = await command.ExecuteAsync();
+            result.Errors.Count().ShouldBe(1);
+        }
+
+        #endregion
+
+        #region Command<T>
+
+        [TestMethod]
+        public void Command_of_T_OnInitialization_Is_Invoked()
+        {
+            var mock = new MockCommandOfString();
+            var result = mock.Execute();
+            mock.OnInitializationWasInvoked.ShouldBe(true);
+        }
+
+        [TestMethod]
+        public void Command_of_T_OnExecute_Is_Invoked_When_No_Errors_Exist()
+        {
+            var mock = new MockCommandOfString();
+            var result = mock.Execute();
+            mock.OnExecuteWasInvoked.ShouldBe(true);
+        }
+
+        [TestMethod]
+        public void Command_of_T_Execute_Returns_Value_When_No_Errors_Exist()
+        {
+            var mock = new MockCommandOfString();
+            var result = mock.Execute();
+            result.Value.ShouldBe("some value");
+        }
+
+        [TestMethod]
+        public void Command_of_T_ExecutionResult_Is_Successful_When_Validation_Is_Successful()
+        {
+            var mock = new MockCommandOfString();
+            var result = mock.Execute();
+            result.Success.ShouldBe(true); 
+        }
+
+        [TestMethod]
+        public void Command_of_T_ExecutionResult_Should_Contain_No_Errors_When_Validation_Is_Successful()
+        {
+            var mock = new MockCommandOfString();
+            var result = mock.Execute();
+            result.Errors.ShouldBe(null);
+        }
+
+        [TestMethod]
+        public void Command_of_T_OnExecute_Is_Not_Invoked_When_Errors_Exist()
+        {
+            var mock = new MockCommandOfString();
+            mock.Errors = new[] { new ValidationResult("Object doesn't exist") };
+            mock.Execute();
+            mock.OnExecuteWasInvoked.ShouldBe(false);
+        }
+
+        [TestMethod]
+        public void Command_of_T_ExecutionResult_Is_Not_Successful_When_Validation_Is_Not_Successful()
+        {
+            var mock = new MockCommandOfString();
+            mock.Errors = new[] { new ValidationResult("Object doesn't exist") };
+            var result = mock.Execute();
+            result.Success.ShouldBe(false); 
+        }
+
+        [TestMethod]
+        public void Command_of_T_ExecutionResult_Should_Contain_Errors_When_Validation_Is_Not_Successful()
+        {
+            var mock = new MockCommandOfString();
+            mock.Errors = new[] { new ValidationResult("Object doesn't exist") };
+            var result = mock.Execute();
+            result.Errors.Count().ShouldBe(1);
+        }
+
+        [TestMethod]
+        public void Command_of_T_ExecutionResult_Should_Contain_Errors_When_ServiceException_Is_Caught()
+        {
+            var command = new MockCommandOfStringThrowsErrorsOnExecute();
+            var result = command.Execute();
+            result.Errors.Count().ShouldBe(1);
+        }
+
+        [TestMethod]
+        public async Task Command_of_T_OnInitializationAsync_Is_Invoked()
+        {
+            var mock = new MockCommandOfString();
+            await mock.ExecuteAsync();
+            mock.OnInitializationAsyncWasInvoked.ShouldBe(true);
+        }
+
+        [TestMethod]
+        public async Task Command_of_T_OnExecuteAsync_Is_Invoked_When_No_Errors_Exist()
+        {
+            var mock = new MockCommandOfString();
+            await mock.ExecuteAsync();
+            mock.OnExecuteAsyncWasInvoked.ShouldBe(true);
+       } 
+
+        [TestMethod]
+        public async Task Command_of_T_Execute_Returns_Value_When_No_Errors_Exist_Async()
+        {
+            var mock = new MockCommandOfString();
+            var result = await mock.ExecuteAsync();
+            result.Value.ShouldBe("some value");
+        }
+
+        [TestMethod]
+        public async Task Command_of_T_OnExecuteAsync_Is_Not_Invoked_When_Errors_Exist()
+        {
+            var mock = new MockCommandOfString();
+            mock.Errors = new[] { new ValidationResult("Object doesn't exist") };
+            await mock.ExecuteAsync();
+            mock.OnExecuteAsyncWasInvoked.ShouldBe(false);
+        }
+
+        [TestMethod]
+        public async Task Command_of_T_ExecutionResult_Should_Contain_Errors_When_ServiceException_Is_Caught_Async()
+        {
+            var command = new MockCommandOfStringThrowsErrorsOnExecute();
+            var result = await command.ExecuteAsync();
+            result.Errors.Count().ShouldBe(1);
+        }
+
+        #endregion
     }
 
     public class MockCommand : Command
@@ -139,6 +281,79 @@ namespace Peasy.Core.Tests
         {
             OnExecuteAsyncWasInvoked = true;
             return Task.FromResult(true);
+        }
+    }
+
+    public class MockCommandThrowsErrorsOnExecute : Command
+    {
+        protected override void OnExecute()
+        {
+            throw new ServiceException("An error occurred");
+        }
+
+        protected override Task OnExecuteAsync()
+        {
+            throw new ServiceException("An error occurred");
+        }
+    }
+
+    public class MockCommandOfString : Command<string>
+    {
+        public MockCommandOfString ()
+        {
+            Errors = Enumerable.Empty<ValidationResult>();
+        }
+
+        public IEnumerable<ValidationResult> Errors { get; set; }
+        public bool OnInitializationWasInvoked { get; set; }
+        public bool OnExecuteWasInvoked { get; set; }
+        public bool OnInitializationAsyncWasInvoked { get; set; }
+        public bool OnExecuteAsyncWasInvoked { get; set; }
+
+        protected override void OnInitialization()
+        {
+            OnInitializationWasInvoked = true;
+        }
+
+        protected override Task OnInitializationAsync()
+        {
+            OnInitializationAsyncWasInvoked = true;
+            return Task.FromResult(true);
+        }
+
+        public override IEnumerable<ValidationResult> GetErrors()
+        {
+            return Errors;
+        }
+
+        public override Task<IEnumerable<ValidationResult>> GetErrorsAsync()
+        {
+            return Task.Run(() => GetErrors());
+        }
+
+        protected override string OnExecute()
+        {
+            OnExecuteWasInvoked = true;
+            return "some value";
+        }
+
+        protected override Task<string> OnExecuteAsync()
+        {
+            OnExecuteAsyncWasInvoked = true;
+            return Task.FromResult("some value");
+        }
+    }
+
+    public class MockCommandOfStringThrowsErrorsOnExecute : Command<string>
+    {
+        protected override string OnExecute()
+        {
+            throw new ServiceException("An error occurred");
+        }
+
+        protected override Task<string> OnExecuteAsync()
+        {
+            throw new ServiceException("An error occurred");
         }
     }
 }
