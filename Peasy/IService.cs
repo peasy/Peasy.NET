674539c8422d0327f2b91ce97649a0f672cd99ca
SlowLogicBoy <SlowLogicBoy@users.noreply.github.com>
2017-09-18 09:52:47 +0300

    Split IService into smaller interfaces

--- Peasy/IService.cs
+++ Peasy/IService.cs
@@ -2,12 +2,36 @@
 
 namespace Peasy
 {
-    public interface IService<T, TKey>
+    public interface IService<T, TKey> : ISupportGetAllCommand<T>, 
+                                         ISupportGetByIDCommand<T, TKey>,
+                                         ISupportInsertCommand<T>,
+                                         ISupportUpdateCommand<T>,
+                                         ISupportDeleteCommand<TKey>
+    {
+    }
+
+    public interface ISupportGetAllCommand<T>
     {
         ICommand<IEnumerable<T>> GetAllCommand();
+    }
+
+    public interface ISupportGetByIDCommand<T, TKey>
+    {
         ICommand<T> GetByIDCommand(TKey id);
+    }
+
+    public interface ISupportInsertCommand<T>
+    {
         ICommand<T> InsertCommand(T entity);
+    }
+
+    public interface ISupportUpdateCommand<T>
+    {
         ICommand<T> UpdateCommand(T entity);
+    }
+
+    public interface ISupportDeleteCommand<TKey>
+    {
         ICommand DeleteCommand(TKey id);
     }
 }
